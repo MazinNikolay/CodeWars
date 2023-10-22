d27e1f1bd2b39f80001f42c68791f2e496b95b38
Nikolay <koliamba_5@mail.ru>
2023-10-22 20:49:45 +0800

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,31 @@
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println(mask("4141589634"));
+        System.out.println(maskArray("4141589634"));
+        System.out.println(maskTernar("4141589634"));
+    }
+
+    public static String mask(String str){
+        StringBuffer sb = new StringBuffer(str);
+        for (int i=0; i<str.length()-4;i++){
+            //System.out.println(str.charAt(i));
+            sb.setCharAt(i,'#');
+        }
+        str=sb.toString();
+        // System.out.println(str);
+        return str;
+    }
+
+    public static String maskArray(String str){
+        char[] chars=str.toCharArray();
+        for(int i=0; i<str.length()-4;i++){
+            chars[i]='#';
+        }
+        str=String.valueOf(chars);
+        return str;
+    }
+
+    public static String maskTernar(String str){
+        return str.length()<4 ? str : str.substring(0,str.length()-4).replaceAll(".","#")+str.substring(str.length()-4);
+    }
+}
